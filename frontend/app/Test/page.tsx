472504432c506577f4a13912eb8cdbee8ecc6b08
17med <ahmedbenachour2003@gmail.com>
2025-12-05 00:09:53 +0100

    feat: update UserStatus component to display user role correctly

--- frontend/app/Test/page.tsx
+++ frontend/app/Test/page.tsx
@@ -4,13 +4,13 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function UserStatus() {
   const { data: session } = useSession();
-  console.log("Session data:", session.realm_roles.includes("admin"));
+
   if (session) {
     return (
       <div>
         <p>
           Signed in as {session.user.email}
-          {session.realm_roles.includes("admin") ? " (Admin)" : ""}
+          {session.realm_roles.includes("admin") ? " (Admin)" : "(user)"}
         </p>
         <button onClick={() => signOut()}>Sign out</button>
       </div>
